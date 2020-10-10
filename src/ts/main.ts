0efbfba699f4148e32ab46f92795da2fdd6fd703
Konstantinos Zagoris <kzagoris@gmail.com>
2020-10-10 08:49:24 +0300

    add js build pipeline

---
+++ src/ts/main.ts
@@ -0,0 +1,14 @@
+import "alpinejs";
+
+declare global {
+  interface Window {
+    toggleNavbar: typeof toggleNavbar;
+  }
+}
+
+function toggleNavbar(collapseID: string) {
+  document.getElementById(collapseID).classList.toggle("hidden");
+  document.getElementById(collapseID).classList.toggle("block");
+}
+
+window.toggleNavbar = toggleNavbar;
